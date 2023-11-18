fed3f97d7028e3a9822a5ed4e23e277995e6dd98
shahnewaz <shahnewaz@example.com>
2023-11-19 01:41:13 +0600

    change the image for carousel

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import "../style/Hero.css";
 import Carousel from "react-bootstrap/Carousel";
-import slide1 from "../assets/slide1.jpg";
-import slide2 from "../assets/slide2.jpg";
-import slide3 from "../assets/slide3.jpg";
-import slide4 from "../assets/slide4.jpg";
-import slide5 from "../assets/slide5.jpg";
+import pic1 from "../assets/pic1.jpg";
+import pic2 from "../assets/pic2.jpg";
+import pic3 from "../assets/pic3.jpg";
 
 function Hero() {
   return (
@@ -13,40 +11,27 @@ function Hero() {
       <Carousel>
         <Carousel.Item>
           <img
-            className="d-block w-100 aspect-ratio-img"
-            src={slide1}
+            className="d-block w-100 "
+            src={pic1}
             alt="First slide"
           />
         </Carousel.Item>
 
         <Carousel.Item>
           <img
-            className="d-block w-100 aspect-ratio-img"
-            src={slide2}
+            className="d-block w-100 "
+            src={pic2}
             alt="Second slide"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100 aspect-ratio-img"
-            src={slide3}
+            className="d-block w-100"
+            src={pic3}
             alt="third slide"
           />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100 aspect-ratio-img"
-            src={slide4}
-            alt="fourth slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100 aspect-ratio-img"
-            src={slide5}
-            alt="fifth slide"
-          />
-        </Carousel.Item>
+          </Carousel.Item>
+       
       </Carousel>
     </div>
   );
